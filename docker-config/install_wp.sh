ba69307a31ab21054d250ed08c96031911dc5f11
Alexandr Shevchenko <ls.shov@gmail.com>
2018-01-25 14:13:04 +0300

    Made some consistence

--- docker-config/install_wp.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker-compose exec php-fpm php ./vendor/bin/wp-cli.phar core install --url='http://localhost' --title='title' --admin_user='admin@test.com' --admin_password='1234qwerrbaetGRGW' --admin_email='admin@test.com' --skip-email --path=wordpress --allow-root --debug
-docker-compose exec php-fpm php ./vendor/bin/wp-cli.phar theme activate rezonans --path=wordpress --allow-root --debug
-docker-compose exec php-fpm php ./vendor/bin/wp-cli.phar plugin activate advanced-custom-fields --path=wordpress --allow-root --debug
-docker-compose exec php-fpm php ./vendor/bin/wp-cli.phar rewrite structure '/%category%/%postname%/' --path=wordpress --allow-root --debug
